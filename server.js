870b227f190e0c0c7d049c7133adeae7e5456be7
Kritsada <atha.kritsada@gmail.com>
2026-01-11 17:01:46 +0700

    update ui and tools

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(
   })
 );
 
+app.use(express.static('public'));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use("/public", express.static(path.join(__dirname, "public")));
