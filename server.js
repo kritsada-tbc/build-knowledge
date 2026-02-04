b4b68316778b32a3d310e268f98671044b60ff4f
Kritsada <atha.kritsada@gmail.com>
2026-02-04 14:36:34 +0700

    ui: change latest section to list style

--- server.js
+++ server.js
@@ -179,9 +179,10 @@ app.use((err, req, res, next) => {
 /* ================= Start ================= */
 initDb()
   .then(() => {
-    app.listen(PORT, () =>
-      console.log(`✅ Server → http://localhost:${PORT}`)
-    );
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server → http://0.0.0.0:${PORT}`);
+});
+
   })
   .catch((e) => {
     console.error("❌ DB init failed", e);
